8936108f3f30f531bf6f120526b1e178b87ffa0f
Majid Jamaleldine <majid.jamaleldine@gmail.com>
2018-06-17 15:23:35 -0500

    setting up mongodb for heroku

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
-const bodyParser = require('body-parser') 
+const bodyParser = require('body-parser');
 const app = express();
-const cheerio = require('cheerio')
-const $ = cheerio.load('<h2 class="title">Hello world</h2>')
+const cheerio = require('cheerio');
+const $ = cheerio.load('<h2 class="title">Hello world</h2>');
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
